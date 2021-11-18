4175e7fc4b506e0c2ea1cb95bf8528f916feb457
njerv7 <84924860+njerv7@users.noreply.github.com>
2021-11-18 13:54:21 +0000

    Created another new script

---
+++ anotherNewScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "this is a script from github"
+NUMBER=$((NUMBERONE + $NUMBERTWO))
+echo "$NUMBER"
+echo "this is a copy of testscript"
+echo "updated script"
+echo "needed to make a change"
+echo "another new script"
+echo "update2"
+echo "update3"
+echo "update4"
